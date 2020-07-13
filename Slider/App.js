2800fc3abf1798d3056f5570f34b6553bb82c32d
Lauryn-gitahi <lauryn.gitahi@gmail.com>
2020-07-13 21:29:20 +0300

    Add files via upload

---
+++ Slider/App.js
@@ -0,0 +1,44 @@
+//selectors
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+// select  buttons
+const prevBtn =  document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+//counter
+let counter=1;//starts at 1st image;
+//select image width
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform= 'translateX(' +(-size * counter) + 'px)';
+
+//button listeners
+nextBtn.addEventListener('click', ()=>{
+  if(counter >= carouselImages.length -1)return;
+  carouselSlide.style.transition= 'transform 0.4s ease-in-out';
+  counter +=1 ;
+  // console.log(counter);
+  carouselSlide.style.transform= 'translateX(' +(-size * counter) +'px)';
+});
+
+prevBtn.addEventListener('click', ()=>{
+  if(counter <=0)return;
+  carouselSlide.style.transition= "transform 0.4s ease-in-out";
+  counter -=1;
+  carouselSlide.style.transform= 'translateX(' +(-size * counter) +'px)';
+
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+  if(carouselImages[counter].id === 'lastClone'){
+      carouselSlide.style.transition= "none";
+      counter = carouselImages.length -2 ;
+      carouselSlide.style.transform= 'translateX(' +(-size * counter) +'px)';
+  }
+    if(carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition= "none";
+        counter = carouselImages.length -counter;
+        carouselSlide.style.transform= 'translateX(' +(-size * counter) +'px)';
+      }
+});
